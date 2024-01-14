c0df810607ea8f4a1259fb2bfe82915b72046a71
Kasun Dushmantha Rangana Herath <141954187+KDRHerath@users.noreply.github.com>
2024-01-14 12:05:03 +0530

    router dom

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,9 +1,11 @@
 import "../../App.css";
 import React from 'react'
+import Navbar from "../NavBar/Navbar";
 
 function Home() {
   return (
-    <div>Home
+    <div>
+      <Navbar/>
     </div>
   )
 }
--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../NavBar/Navbar';
 
-export default function Home() {
+export default function () {
   return (
-    <div>Home</div>
+    <div>
+        <Navbar/>
+    </div>
   )
 }
