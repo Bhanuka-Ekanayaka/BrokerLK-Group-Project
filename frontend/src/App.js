09d673228e9c1fa7e4058bf6b0c7a629506beed2
Mohammed Asran <mohammedasran@Mohammeds-MacBook-Air.local>
2024-01-15 17:12:02 +0530

    forgot password added and seller section added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home'
+import './App.css'
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
 import Profile from './Components/Profile/Profile';
+import Sell from './Components/Seller/Sell';
+import ForgotPassword from './Components/Login/ForgotPassword';
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
           <Route path='/login' element={<Login/>} />
           <Route path='/register' element={<Register/>} />
           <Route path='/profile' element={<Profile/>}/>
+          <Route path='/seller' element={<Sell/>}/>
+          <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
     </Router>
     </div>
