5c22c158b8609b332723871885965ff38b173662
Mohammed Asran <mohammedasran@Mohammeds-MacBook-Air.local>
2024-02-27 23:26:08 +0530

    conflict solve

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -4,38 +4,25 @@ import Catergory from './Child-Catergory/Catergory';
 import { showSuccessToast, showErrorToast, CommonToastContainer } from '../../Services/CommonToaster';
 import AutoLayout from '../Card/AutoLayout';
 import './home.css';
-
 import SearchBar from './Child-SearchBar/SearchBar';
 import Cards from './Child-Card/Cards';
 import Footer from '../Child/Footer/Footer';
 
-<<<<<<< HEAD
 const Home = (updateAuthentication) => {
 
   const toggleUpdateAuthentication = () => {
     updateAuthentication(false)
   };
-=======
-
-
-
-const Home = ({isAuthenticated}) => {
->>>>>>> main
-
-
-
-
-  
 
   return (
     <>
       <NavBar toggleUpdateAuthentication={toggleUpdateAuthentication}/>
       <SlideShow/>
       <Catergory />
-      <SearchBar />
-      <Cards title='Boarding Rooms' animation='flip-right' CardAnimate='flip-down' type='boarding-room'/>
-      <Cards title='Rental Houses'  animation='flip-left' CardAnimate='flip-up' type='rental-house' />
-      <Cards title='Boarding Building'  animation='flip-right' CardAnimate='flip-down' type='boarding-building'/>
+      <SearchBar/>
+      <Cards title='Boarding Building' animation='flip-right' CardAnimate='flip-down' type='boarding-room'/>
+      <Cards title='Rental Houses'  animation='flip-left' CardAnimate='flip-up' type='boarding-room' />
+      <Cards title='Boarding Rooms'  animation='flip-right' CardAnimate='flip-down' type='boarding-room'/>
       <Footer animation="flip-right"/>
     </>
   );
